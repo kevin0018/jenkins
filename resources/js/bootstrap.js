c0133baaefd38fbf2c731c61237241ebfcdd37fa
Kevin <1hiaw.hernandez.kevin@gmail.com>
2024-03-07 14:30:37 +0100

    menu

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+import '@fortawesome/fontawesome-free/css/all.css';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- resources/views/layouts/cine.blade.php
+++ resources/views/layouts/cine.blade.php
@@ -7,14 +7,58 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 <body class="font-sans antialiased bg-dark text-white">
-    <menu class="navbar navbar-expand-lg navbar-light bg-light">
-        <img class="navbar-brand" src="{{URL::asset('/img/Jenkins_logo.png')}}" alt="LogoCine" width="200px">
-        
-        <input type="text" placeholder="Search">
-        <a href="">Log in</a>
-    </menu>
+
+
+    <!-- Menu -->
+    <header>
+        <nav class="navbar navbar-default bg-warning">
+            <div class="container">
+                <!-- Logo -->
+                <div class="navbar-header">
+                    {{-- <button type="button" class="navbar-toggle collapsed" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only"></span>
+                        <span class="icon-bar"></span>
+                    </button> --}}
+                    
+                    <a href="/" class="navbar-brand">
+                        <img src="{{URL::asset('/img/Jenkins_logo.png')}}" alt="LogoCine" width="200px">
+                    </a>
+                </div>
+                <!-- Final Logo -->
+
+                <!-- Botones de la derecha -->
+                {{-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> --}}
+                <div class=" navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Peliculas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Log in</a>
+                        </li>
+                        <li class="nav-item">
+                            <form class="form-inline">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="btn-search">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="button" id="btn-search">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </div>
+                            </form>
+                        </li>
+                    </ul>
+                <!-- Final de los botones -->
+            </div>
+        </nav>
+    </header>
+
     <main class="container mx-auto" style="width: 85%;">
         @yield('content')
     </main>
+
 </body>
 </html>
