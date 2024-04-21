d52520a5c0541298c0df26ceafbfbb64d5df06cc
Kevin <1hiaw.hernandez.kevin@gmail.com>
2024-04-22 01:31:49 +0200

    serie list arreglado

--- resources/views/cine/lista_serie.blade.php
+++ resources/views/cine/lista_serie.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.cine')
-
 @section('title', 'Series List')
-
 @section('content')
-    <h1 class="with-line">SERIES LIST</h1>
-    <div class="content py-2 movie-list">
-        @foreach($series as $serie)
-            <a href="{{ route('serie_capitulos', $serie->id) }}">
-                <img src="{{ asset('storage/' . $serie->caratula) }}" alt="{{ $serie->nombre }}">
-                <p>{{ $serie->nombre }}</p>
-            </a>
-        @endforeach
-    </div>
+    @foreach($series as $serie)
+        <h1 class="with-line">SERIES LIST</h1>
+        <div class="content py-2 movie-list">
+            
+                <a href="{{ route('serie_capitulos', $serie->id) }}">
+                    <img src="{{ asset('storage/' . $serie->caratula) }}" alt="{{ $serie->nombre }}">
+                    <p>{{ $serie->nombre }}</p>
+                </a>
+        </div>
+    @endforeach
+@endsection
