35fcf4491bcc89aa8f44974cca90290c22713123
Kevin <1hiaw.hernandez.kevin@gmail.com>
2024-04-22 10:50:22 +0200

    public movie_poster

--- database/seeders/PeliculasTableSeeder.php
+++ database/seeders/PeliculasTableSeeder.php
@@ -15,5 +15,16 @@ class PeliculasTableSeeder extends Seeder
     public function run()
     {
         //
+        Media::create([
+            'titulo_medio' => 'Kung Fu Panda 4',
+            'sinopsis' => "Animated movie for family audiences that is the fourth installment of the Kung Fu Panda franchise. It's time for Po to take the next step in his journey. After years of being the Dragon Warrior alongside Master Shifu, the martial arts expert panda will ascend to Spirit Leader of the Valley of Peace. To achieve this, he needs to find and train a new Dragon Warrior. ",
+            'caratula' => , // Genera una URL de imagen aleatoria
+            'genero' => $faker->randomElement(['Action', 'Comedy', 'Drama', 'Sci-fi', 'Fantasy']),
+            'duracion_total' => $faker->numberBetween(60, 240),
+            'link' => $faker->imageUrl(),
+            'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => now(),
+
+        ]);
     }
 }
